b60c3d7457c9cb4eb8bbe08daacdd964a0cdb75f
방소윤 <bbangso0322@gmail.com>
2021-01-12 23:26:45 +0900

    Form usage 2

--- django_intro/boards/views.py
+++ django_intro/boards/views.py
@@ -6,3 +6,17 @@ def index(request):
 
 def new(request):
     return render(request, 'boards/new.html')
+
+def complete(request):
+    # request는 요청과 관련된 정보들이 담긴 객체 이다
+    # print(request.GET)
+    # print(request.method)
+    # print(request.path)
+
+    title = request.GET.get('title')
+    content = request.GET.get('content')
+    context = {
+        'title' : title,
+        'content' : content
+    }
+    return render(request, 'boards/complete.html', context)
