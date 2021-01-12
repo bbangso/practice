b40f27c69d36c19456a1244d4d7eca10cb9c19cf
방소윤 <bbangso0322@gmail.com>
2021-01-12 14:41:57 +0900

    Setting

--- django_intro/django_intro/settings.py
+++ django_intro/django_intro/settings.py
@@ -106,13 +106,13 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'ko-KR'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
-USE_I18N = True
+USE_I18N = True # Internationalization
 
-USE_L10N = True
+USE_L10N = True # 
 
-USE_TZ = True
+USE_TZ = True #timezone
 
 
 # Static files (CSS, JavaScript, Images)
