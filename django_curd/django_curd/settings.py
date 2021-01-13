5df8b4050fe9f3c4293b0e2cd49251ee68f88384
방소윤 <bbangso0322@gmail.com>
2021-01-13 13:43:40 +0900

    start articles app

--- django_curd/django_curd/settings.py
+++ django_curd/django_curd/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '^$4*2(bbbz#lt+&rfv8b5-&^gc9f9#c8jdqgqw!=fqy(g_(z$%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'articles',
 ]
 
 MIDDLEWARE = [
@@ -103,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
