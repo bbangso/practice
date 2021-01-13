1ae918ae6a511836315ce42d98812da29cec8cb6
방소윤 <bbangso0322@gmail.com>
2021-01-13 15:44:51 +0900

    Redirect

--- django_curd/articles/views.py
+++ django_curd/articles/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Article
 
 # Create your views here.
@@ -18,4 +18,4 @@ def create(request):
     article.title = request.GET.get('title')
     article.content = request.GET.get('content')
     article.save()
-    return render(request, 'articles/create.html')
+    return redirect('/articles/')
