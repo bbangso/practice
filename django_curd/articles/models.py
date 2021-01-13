0e3a3f93ce61f76328ec21f23589672334b552d2
방소윤 <bbangso0322@gmail.com>
2021-01-13 14:09:45 +0900

    Create Article model

--- django_curd/articles/models.py
+++ django_curd/articles/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=140)
+    content = models.TextField()
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
+
+    
