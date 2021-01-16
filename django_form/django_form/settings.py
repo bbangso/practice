49556cf3bb7eb15202af055615bae3a76435b11a
방소윤 <bbangso0322@gmail.com>
2021-01-16 23:27:29 +0900

    Create new app, articles and register

--- django_form/django_form/settings.py
+++ django_form/django_form/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'articles',
 ]
 
 MIDDLEWARE = [
@@ -56,7 +57,9 @@ ROOT_URLCONF = 'django_form.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.join
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
