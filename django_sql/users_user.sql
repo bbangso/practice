754a64745fbd39ed30fc4e6a29eb6f11c823adf1
방소윤 <bbangso0322@gmail.com>
2021-01-16 20:17:18 +0900

    sql

---
+++ django_sql/users_user.sql
@@ -0,0 +1,13 @@
+-- users_user
+
+CREATE TABLE "users_user"(
+    "id" integer NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "first_name" varchar(10) NOT NULL,
+    "last_name" varchar(10) NOT NULL,
+    "age" integer NOT NULL,
+    "country" varchar(10) NOT NULL,
+    "phone" varchar(15) NOT NULL,
+    "balance" integer NOT NULL
+);
+
+-- .read users_user.sql
