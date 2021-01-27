0ca508176408155b0e60ba2dfbcda0c0ab42c598
방소윤 <bbangso0322@gmail.com>
2021-01-27 21:09:34 +0900

    feat: Create new app

---
+++ django/django_sql/articles/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=10)
+    content = models.TextField()
+
+class Reporter(models.Model):
+    username = models.CharField(max_length=10)
