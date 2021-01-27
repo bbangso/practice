a66f76d9a24b352ebc1b2ad72d0ece1b2fc583e8
방소윤 <bbangso0322@gmail.com>
2021-01-27 21:29:29 +0900

    feat: create articles model

--- django/django_sql/articles/models.py
+++ django/django_sql/articles/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Reporter(models.Model):
+    username = models.CharField(max_length=10)
+
 class Article(models.Model):
     title = models.CharField(max_length=10)
     content = models.TextField()
-
-class Reporter(models.Model):
-    username = models.CharField(max_length=10)
+    reporter = models.ForeignKey(Reporter, on_delete=models.CASCADE)
