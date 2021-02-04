afb2be8c9e5dcb372a3ee304ad381f00ac9fbf04
방소윤 <bbangso0322@gmail.com>
2021-02-04 22:50:03 +0900

    feat: modeling, serializer, list view

---
+++ django/api/blog/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import Article, Comment
+
+class ArticleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Article
+        fields = '__all__'
--- django/api/blog/views.py
+++ django/api/blog/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import  get_object_or_404
+
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+from .serializers import ArticleSerializer
+from .models import Article
 
 # Create your views here.
+
+@api_view(['GET'])
+def article_list(request):
+    articles = Article.objects.all()
+    serializer = ArticleSerializer(articles, many=True)    
+
+    return Response(serializer.data)
