6880430ea00695ed4cb0c90be470413519341ac1
방소윤 <bbangso0322@gmail.com>
2021-02-04 17:20:43 +0900

    feat: json api_2

--- django/api/board/urls.py
+++ django/api/board/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('json1', views.article_list_1),
+    path('json2', views.article_list_2),
 ]
--- django/api/board/views.py
+++ django/api/board/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
+from django.views.decorators.http import require_GET
 from django.http.response import JsonResponse
 from .models import Article
 
 # Create your views here.
 
+@require_GET
 def article_list_1(request):
     articles = Article.objects.all()
     data = []
@@ -12,4 +14,11 @@ def article_list_1(request):
             "title" : article.title,
             "content" : article.content,
         })
+    return JsonResponse(data, safe=False)
+
+@require_GET
+def article_list_2(request):
+    from django.core import serializers
+    articles = Article.objects.all()
+    data = serializers.serialize('json', articles)
     return JsonResponse(data, safe=False)
