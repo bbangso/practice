33df6d058cc464a5ffe7fbf932c05eb1c28ca604
방소윤 <bbangso0322@gmail.com>
2021-02-14 18:29:49 +0900

    vue router

---
+++ vue/vue_first/src/views/Contact.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="contact">
+		<h1>Contact ME!</h1>
+		<p>
+			연락주세요
+		</p>
+  </div>
+</template>
+
+<script>
+export default {
+	name: 'Contact',
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue/vue_first/src/views/Ping.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>Ping</h1>
+    <input @keyup.enter="sendToPong" v-model="inputText" type="text">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Ping',
+  data() {
+    return {
+      inputText: '',
+    }
+  },
+  methods: {
+    sendToPong() {
+      this.$router.push({
+        name:'Pong',
+        query: {
+          message: this.inputText
+        }
+      })
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
